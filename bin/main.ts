83c2ab6bf185a2ff84a7d40080f409a426e2fb3b
ajbergh <53879585+ajbergh@users.noreply.github.com>
2022-03-24 09:16:31 -0500

    Update main.ts

--- bin/main.ts
+++ bin/main.ts
@@ -1,6 +1,6 @@
 import { App } from '@aws-cdk/core';
 import * as ssp from '@aws-quickstart/ssp-amazon-eks';
-import { KastenK10AddOn } from '@kastenhq/kasten-aws-quickstart';
+import { KastenK10AddOn } from '../dist;
 
 const app = new App();
 
@@ -10,4 +10,4 @@ ssp.EksBlueprint.builder()
     .addOns(new ssp.addons.SSMAgentAddOn) // needed for AWS internal accounts only
     .addOns(new ssp.SecretsStoreAddOn) // requires to support CSI Secrets
      .addOns(new KastenK10AddOn)
-     .build(app, 'my-extension-test-blueprint');
+     .build(app, 'kasten-extension-test-blueprint');
